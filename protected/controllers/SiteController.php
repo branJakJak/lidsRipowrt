02a486070282621cabe4a803fe054c7f89bb064c
kebyang <kelvo_link@yahoo.com>
2016-01-12 03:39:47 +0800

    added filter for index

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,6 +2,39 @@
 
 class SiteController extends Controller
 {
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', 
+		);
+	}
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array(
+				'allow',
+				'actions'=>array('error','login','logout'),
+				'users'=>array('*'),
+			),
+			array(
+				'allow',
+				'actions'=>array('index'),
+				'users'=>array('@'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}	
 	/**
 	 * Declares class-based actions.
 	 */
